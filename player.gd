fcabd4062c54d1a3212a3e20ce76db09586538d5
purplecurtain23 <shahira.azhar23@gmail.com>
2026-08-23 17:48:39 -0700

    light added

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
+@export var SPEED = 14
+#@export var JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
@@ -10,9 +10,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	## Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
